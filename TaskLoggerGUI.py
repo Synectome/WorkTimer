6a75cf50782de446d9b4de51c30975e0cc0964dd
Synectome <jessedgb@gmail.com>
2020-07-30 17:12:38 -0400

    start button initialized

--- TaskLoggerGUI.py
+++ TaskLoggerGUI.py
@@ -4,9 +4,9 @@ import csv
 import datetime
 
 
-def task_submition():
-    task_name = my_entry.get()
-    description = my_entry2.get()
+def task_submition(entr1, entr2, toor):
+    task_name = entr1.get()
+    description = entr2.get()
     logtime = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     data = [logtime, task_name, description]
     print(task_name)
@@ -23,24 +23,27 @@ def task_submition():
         with open(filename, 'w') as file:
             writer = csv.writer(file)
             writer.writerows([["Time", "TaskName", "Description"], data])
+    toor.destroy()
 
 
-root = Tk()
-root.geometry("300x300")
+def task_log_window():
+    root = Tk()
+    root.geometry("300x300")
 
-frame = Frame(root)
-frame.pack()
+    frame = Frame(root)
+    frame.pack()
 
-my_entry = Entry(frame, width=20)
-my_entry.insert(0, 'Task Name')
-my_entry.pack(padx=5, pady=5)
+    my_entry = Entry(frame, width=20)
+    my_entry.insert(0, 'Task Name')
+    my_entry.pack(padx=5, pady=5)
 
-my_entry2 = Entry(frame, width=30)
-my_entry2.insert(0, 'Description')
-my_entry2.pack(padx=5, pady=5)
+    my_entry2 = Entry(frame, width=30)
+    my_entry2.insert(0, 'Description')
+    my_entry2.pack(padx=5, pady=5)
 
-button1 = Button(frame, text="Submit Log", width=8, bg="#18d9ac", command=task_submition)
-button1.pack(padx=3, pady=3)
+    button1 = Button(frame, text="Submit Log", width=8, bg="#18d9ac",
+                     command=lambda: task_submition(my_entry, my_entry2, root))
+    button1.pack(padx=3, pady=3)
 
-root.title("Log Entry Form")
-root.mainloop()
+    root.title("Log Entry Form")
+    root.mainloop()
--- TimerGUI.py
+++ TimerGUI.py
@@ -1,11 +1,38 @@
 from tkinter import *
+from TaskLoggerGUI import *
 import datetime
+
+init_time = 0
+timing_on = False
+
+def start_timer():
+    global init_time
+    global timing_on
+    timing_on = True
+    init_time = datetime.datetime.now()
+
+
+def time_since():
+    cwt = datetime.datetime.now()
+    global timing_on
+    print(timing_on)
+    if timing_on:
+        since = cwt - init_time
+        seconds = since.seconds
+        hours, remainder = divmod(seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        stringify = '{:02}:{:02}:{:02}'.format(int(hours), int(minutes), int(seconds)) #since.strftime("%H:%M:%S")
+        lbl2.config(text=stringify)
+        lbl2.after(1000,time_since)
+    else:
+        lbl2.config(text='--/--/--')
+        lbl2.after(1000,time_since)
 
 
 def time():
     time_string = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    lbl.config(text=time_string)
-    lbl.after(1000, time)
+    lbl1.config(text=time_string)
+    lbl1.after(1000, time)
 
 
 root = Tk()
@@ -23,20 +50,26 @@ rightframe.pack(side=RIGHT)
 label = Label(frame, text="Your on the clock")
 label.pack(pady=4)
 
-button1 = Button(leftframe, text="Start", width=8, bg="#18d9ac")
+button1 = Button(leftframe, text="Start", width=8, bg="#18d9ac", command=start_timer)
 button1.pack(padx=3, pady=3)
+lbl2 = Label(leftframe, font=('calibri', 12, 'bold'),
+             background="#18d9ac",
+             foreground='black',
+             height=1)
+lbl2.pack(padx=3, pady=3)
+time_since()
 button2 = Button(leftframe, text="Pause", width=8, bg="#18d9ac")
 button2.pack(padx=3, pady=3)
-button3 = Button(leftframe, text="Log Task", width=8, bg="#18d9ac")
+button3 = Button(leftframe, text="Log Task", width=8, bg="#18d9ac", command=task_log_window)
 button3.pack(padx=3, pady=3)
 button4 = Button(leftframe, text="Stop", width=8, bg="#18d9ac")
 button4.pack(padx=3, pady=3)
 
-lbl = Label(root, font=('calibri', 40, 'bold'),
+lbl1 = Label(root, font=('calibri', 40, 'bold'),
             background='#38ba41',
             foreground='black',
             height=2)
-lbl.pack(anchor='center', pady=12)
+lbl1.pack(anchor='center', pady=12)
 time()
 
 root.title("Work Timer")
